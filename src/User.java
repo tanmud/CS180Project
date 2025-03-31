eeaacaa3bfc273754a37f8ffa443474db3bbbaa1
natlam2028 <nl2028ca@gmail.com>
2025-03-31 12:40:48 -0400

    added methods to send messages and get chats

--- src/User.java
+++ src/User.java
@@ -5,8 +5,10 @@ public interface User {
     // Bank bank
     // Constructor should create name, username, password, set bank with everything 0/null, friends null.
     //       User user = this; in each class
-    //
-    boolean message(User user);
+    // ArrayList<Message>
+    String sendMessage(Message message); // sends message and returns if it was successful or not; adds to chat arraylist
+//    boolean message(User user);
+    String getChat(User recipient); // get message history with specified person; check if it's a chat
     boolean deleteAccount();
 
     User getName();
