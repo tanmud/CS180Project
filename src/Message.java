e096a09b2ea86aa2a0ebf97255a24f6627f57ea8
natlam2028 <nl2028ca@gmail.com>
2025-03-31 12:19:44 -0400

    Update Message.java
    added sendMessage and getChat methods + comments

--- src/Message.java
+++ src/Message.java
@@ -4,5 +4,6 @@ public interface Message {
     // String content
     // String Date            in format {Month,Day,Year}
     // String time            in format {hour:min} 24 hour clock
-
+    String sendMessage(Message message); // sends message and returns if it was successful or not; writes to chats file
+    String getChat(User recipient); // get message history with specified person
 }
