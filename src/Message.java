74114ee7d99b057d936bbf3157a0e6fa76095336
natlam2028 <nl2028ca@gmail.com>
2025-03-31 12:41:11 -0400

    clarified and added possible method

--- src/Message.java
+++ src/Message.java
@@ -4,6 +4,6 @@ public interface Message {
     // String content
     // String Date            in format {Month,Day,Year}
     // String time            in format {hour:min} 24 hour clock
-    String sendMessage(Message message); // sends message and returns if it was successful or not; writes to chats file
-    String getChat(User recipient); // get message history with specified person
+
+    // add the getters and setters for properties!
 }
