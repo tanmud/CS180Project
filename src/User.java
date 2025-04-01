8e34af57aa0a1a936e3d1912721cca5724a08474
natlam2028 <nl2028ca@gmail.com>
2025-04-01 01:27:45 -0400

    Coded some methods in User.java
    not done yet, but pasting for now just to keep the github updated

---
+++ src/User.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+public class User extends Bank implements UserInterface {
+
+
+    private String name;
+    private String username;
+    private String password;
+    private Bank bank;
+    private ArrayList<Message> chats;
+
+    // Constructor should create name, username, password, set bank with everything 0/null
+    //       User.java user = this; in each class
+    // ArrayList<Message> chats
+
+    public User(String name, String username, String password, Bank bank) {
+
+        super();
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String sendMessage(Message message) {
+
+        int i = 0;
+        for (int j = 0; j < this.chats.size(); j++) {
+
+            if (this.chats.get(j).getRecipient().equals(message.getRecipient())) {
+
+                i = j;
+            }
+        }
+        this.chats.add(i, message);
+        return "Message sent successfully!";
+    } // sends message and returns if it was successful or not; adds to chat arraylist
+    //    boolean message(User.java user);
+    public ArrayList<Message> getChats() {
+
+        return this.chats;
+    }
+
+    public ArrayList<Message> getChat(User recipient) throws NoChatFoundException {
+
+        ArrayList<Message> chat = this.chats;
+        for (int j = 0; j < this.chats.size(); j++) {
+
+            if (this.chats.get(j).getRecipient().equals(recipient)) {
+
+                chat.subList(j + 1, chat.size()).clear();
+            }
+        }
+        if (chat.equals(this.chats)) {
+
+            throw new NoChatFoundException("You have no chat with this user!");
+        }
+    } // get message history with specified person; check if it's a chat
+    // have an exception with chat doesn't exist
+    public void deleteAccount() {
+
+
+    } // set everything to default null values; delete message history, etc.
+
+
+    // rest of the getter & setters
+    public String getName() {
+
+
+    }
+    public void setName(String name) {
+
+
+    }
+
+    public String getUsername() {
+
+
+    }
+    public void setUsername(String username) {
+
+
+    }
+
+    public String getPassword(){
+
+
+    }
+    public void setPassword(String password){
+
+
+    }
+}
