6798463905679ec3730d65ccc8d3a07f4ab1bae9
natlam2028 <nl2028ca@gmail.com>
2025-03-31 23:58:54 -0400

    added basic getters and setters to MessageInterface

--- src/MessageInterface.java
+++ src/MessageInterface.java
@@ -6,4 +6,18 @@ public interface MessageInterface {
     // String time            in format {hour:min} 24 hour clock
 
     // add the getters and setters for properties!
+    User getSender();
+    void setSender(User sender);
+
+    User getRecipient();
+    void setRecipient(User recipient);
+
+    String getContent();
+    void setContent(String content);
+
+    String getDate();
+    void setDate(LocalDate date);
+
+    String getTime();
+    void setTime(LocalTime time);
 }
