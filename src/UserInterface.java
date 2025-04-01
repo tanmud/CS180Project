d94febc41edda1e259967704aeb56bf6720ed71b
natlam2028 <nl2028ca@gmail.com>
2025-04-01 01:27:12 -0400

    revised some methods in UserInterface

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public interface UserInterface {
 
     // extends Bank?
@@ -7,14 +9,14 @@ public interface UserInterface {
     // Password
     // Bank bank
     // Constructor should create name, username, password, set bank with everything 0/null
-    //       User user = this; in each class
+    //       User.java user = this; in each class
     // ArrayList<Message> chats
     String sendMessage(Message message); // sends message and returns if it was successful or not; adds to chat arraylist
-//    boolean message(User user);
-    String getChat(User recipient); // get message history with specified person; check if it's a chat
-    boolean deleteAccount(); // set everything to default null values; delete message history, etc.
+    //    boolean message(User.java user);
+    ArrayList<Message> getChat(User recipient) throws NoChatFoundException; // get message history with specified person; check if it's a chat
+    void deleteAccount(); // set everything to default null values; delete message history, etc.
 
-    User getName();
+//    User getName();
 
     // rest of the getter & setters
     String getName();
@@ -26,6 +28,6 @@ public interface UserInterface {
     String getPassword();
     void setPassword(String password);
 
-    String getChats(); // convert chat list to readable format
+    ArrayList<Message> getChats(); // convert chat list to readable format
     // setchat is essentially the sendMessage
 }
