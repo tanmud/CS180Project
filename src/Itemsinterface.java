52a1d0b86dcab4720fed6ffbe4c2cf1461d64257
bancroft24 <bancroft@purdue.edu>
2025-04-03 11:43:20 -0400

    Update Itemsinterface.java

--- src/Itemsinterface.java
+++ src/Itemsinterface.java
@@ -3,4 +3,19 @@ public interface ItemsInterface {
     // String Description
     // int quantity
     // User user
+
+    // getters and setters
+    String getName();
+    void setName(String user);
+
+    String getDescription();
+    void setDescription(String description);
+
+
+    int getQuantity();
+    void setQuantity(int quantity);
+
+    User getUser();
+    void setUser(User user);
 }
+
