783d0c9d516f020661189c8c62b7bff39486e001
natlam2028 <nl2028ca@gmail.com>
2025-04-06 00:00:41 -0400

    added MessageTest

---
+++ src/MessageTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MessageTest {
+
+    private LocalDate date = LocalDate.of(2025, 04, 05);
+    private LocalTime time =  LocalTime.of(23, 33, 01);
+    User u1 = null;
+    User u2 = null;
+    Message testMessage = new Message(u1, u2, "test");
+
+
+    // test dateToString
+    @Test
+    public void convertedDateReturnsDetailedFormat() {
+
+        compareExpectedDateWithResult();
+    }
+
+    private void compareExpectedDateWithResult() {
+
+        testMessage.setDate(this.date);
+        String newDate = testMessage.dateToString();
+        assertEquals("APRIL 5, 2025", newDate);
+    }
+
+    // test dateToString
+    @Test
+    public void convertedTimeReturnsDetailedFormat() {
+
+        compareExpectedTimeWithResult();
+    }
+
+    private void compareExpectedTimeWithResult() {
+
+        testMessage.setTime(this.time);
+        String newTime = testMessage.timeToString();
+        assertEquals("23:33", newTime);
+    }
+
+
+}
